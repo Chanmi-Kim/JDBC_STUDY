a90222cf0589cf9ed0f7305824be5eaaeec266c4
xxHANIxx <kimcm0319@gmail.com>
2019-04-12 14:28:29 +0900

    Add File [Member.java]
    1. JAVA 파일 추가
    
    - Member.java 파일 추가
      (JDBCTest\test\Member.java)

---
+++ JDBCTest/test/Member.java
@@ -0,0 +1,15 @@
+﻿package test;
+
+//Member extends IMember //부모 : 일반, 추상
+//Member implements IMember //부모 : 인터페이스
+
+public class Member implements IMember {
+	
+
+	@Override //재정의 - 다시 선언하겠습니다.
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
